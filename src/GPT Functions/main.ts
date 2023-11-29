9e1a6308fd86c42a416d02d342ae3227cc279073
RacquelNinaDennison <racquelnina@gmail.com>
2023-11-29 12:02:34 +0200

    Added modified files

--- src/GPT Functions/main.ts
+++ src/GPT Functions/main.ts
@@ -1,6 +1,17 @@
 import { createAssistant } from "./createAssistant";
 import { extractHtmlFromAssistantAndWriteToFile } from "./extractHtml";
+import simpleGit, { SimpleGit } from "simple-git";
 
+const repoPath =
+	"https://github.com/RacquelNinaDennison/gpt-website-maker.git";
+const git: SimpleGit = simpleGit(repoPath);
+const commitMessage = "Added the changed index.html file";
+
+const runGit = async () => {
+	await git.add("./src/HTML/index.html"); // Stage all changes
+	await git.commit(commitMessage);
+	console.log("Added to git reposioty");
+};
 export const run = async () => {
 	await createAssistant();
 	extractHtmlFromAssistantAndWriteToFile();
