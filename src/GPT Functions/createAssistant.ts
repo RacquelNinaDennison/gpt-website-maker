71f30146edf6a1289002b0ee6cace03d5079e6d8
RacquelNinaDennison <racquelnina@gmail.com>
2023-12-01 11:06:52 +0200

    Added try catch

--- src/GPT Functions/createAssistant.ts
+++ src/GPT Functions/createAssistant.ts
@@ -8,48 +8,51 @@ dotenv.config();
 const openai = new OpenAI({ apiKey: process.env.API_KEY });
 
 export const createAssistant = async () => {
-	const myWebpageAssistant =
-		await openai.beta.assistants.retrieve(
-			"asst_FAafr8Epv5OiUu8DJwvZ5923"
-		);
-
-	const thread = await openai.beta.threads.create();
-	console.log(userDescriptionText);
-
-	await openai.beta.threads.messages.create(thread.id, {
-		role: "user",
-		content: userDescriptionText,
-	});
-
-	const run = await openai.beta.threads.runs.create(
-		thread.id,
-		{
-			assistant_id: myWebpageAssistant.id,
-		}
-	);
-
-	while (true) {
-		const runMessages =
-			await openai.beta.threads.runs.retrieve(
-				thread.id,
-				run.id
+	try {
+		const myWebpageAssistant =
+			await openai.beta.assistants.retrieve(
+				"asst_FAafr8Epv5OiUu8DJwvZ5923"
 			);
 
-		if (runMessages.status == "completed") {
-			console.log("Completed");
-			break;
+		const thread = await openai.beta.threads.create();
+		console.log(userDescriptionText);
+
+		await openai.beta.threads.messages.create(thread.id, {
+			role: "user",
+			content: userDescriptionText,
+		});
+
+		const run = await openai.beta.threads.runs.create(
+			thread.id,
+			{
+				assistant_id: myWebpageAssistant.id,
+			}
+		);
+
+		while (true) {
+			const runMessages =
+				await openai.beta.threads.runs.retrieve(
+					thread.id,
+					run.id
+				);
+
+			if (runMessages.status == "completed") {
+				console.log("Completed");
+				break;
+			}
+			console.log("Waiting for the assistant");
+			sleep(3000);
 		}
-		console.log("Waiting for the assistant");
-		sleep(3000);
+
+		const messages =
+			await openai.beta.threads.messages.list(thread.id);
+		const message = messages.data[0].content[0];
+		console.log(message);
+
+		const htmlFile =
+			extractHtmlFromAssistantAsAVariable(message);
+		return htmlFile;
+	} catch (error) {
+		console.log(error);
 	}
-
-	const messages = await openai.beta.threads.messages.list(
-		thread.id
-	);
-	const message = messages.data[0].content[0];
-	console.log(message);
-
-	const htmlFile =
-		extractHtmlFromAssistantAsAVariable(message);
-	return htmlFile;
 };
