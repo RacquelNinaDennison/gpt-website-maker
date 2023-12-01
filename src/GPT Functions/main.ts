6cc8429723c1afd18e85a3003cdc08676c64389d
RacquelNinaDennison <racquelnina@gmail.com>
2023-12-01 11:22:11 +0200

    Added try catch

--- src/GPT Functions/main.ts
+++ src/GPT Functions/main.ts
@@ -6,45 +6,61 @@ import { createAssistant } from "./createAssistant";
 
 const runGit = async (
 	username: string,
-	contentHtml: string
+	contentHtml: string | undefined
 ): Promise<string> => {
-	const account = process.env.ACCOUNT_NAME as string;
-	const accountKey = process.env.ACCOUNT_KEY as string;
-	const containerName = "$web";
+	try {
+		if (contentHtml) {
+			const account = process.env.ACCOUNT_NAME as string;
+			const accountKey = process.env.ACCOUNT_KEY as string;
+			const containerName = "$web";
 
-	const sharedKeyCredential =
-		new StorageSharedKeyCredential(account, accountKey);
-	const blobServiceClient = new BlobServiceClient(
-		`https://${account}.blob.core.windows.net`,
-		sharedKeyCredential
-	);
-	const containerClient =
-		blobServiceClient.getContainerClient(containerName);
+			const sharedKeyCredential =
+				new StorageSharedKeyCredential(account, accountKey);
+			const blobServiceClient = new BlobServiceClient(
+				`https://${account}.blob.core.windows.net`,
+				sharedKeyCredential
+			);
+			const containerClient =
+				blobServiceClient.getContainerClient(containerName);
 
-	const content = contentHtml;
-	const blobName = `${username}+${new Date().getTime()}/${new Date().getTime()}+index.html`;
-	const blockBlobClient =
-		containerClient.getBlockBlobClient(blobName);
-	const blobOptions = {
-		blobHTTPHeaders: { blobContentType: "text/html" },
-	};
-	const uploadBlobResponse = await blockBlobClient.upload(
-		content,
-		content.length,
-		blobOptions
-	);
+			const content = contentHtml;
+			const blobName = `${username}+${new Date().getTime()}/${new Date().getTime()}+index.html`;
+			const blockBlobClient =
+				containerClient.getBlockBlobClient(blobName);
+			const blobOptions = {
+				blobHTTPHeaders: { blobContentType: "text/html" },
+			};
+			const uploadBlobResponse =
+				await blockBlobClient.upload(
+					content,
+					content.length,
+					blobOptions
+				);
 
-	console.log(
-		`Upload block blob ${blobName} successfully. Client request id ${uploadBlobResponse.clientRequestId}`,
-		uploadBlobResponse.requestId
-	);
-	const blobUrl = blockBlobClient.url;
-	console.log(`Blob URL: ${blobUrl}`);
-	return blobUrl;
+			console.log(
+				`Upload block blob ${blobName} successfully. Client request id ${uploadBlobResponse.clientRequestId}`,
+				uploadBlobResponse.requestId
+			);
+			const blobUrl = blockBlobClient.url;
+			console.log(`Blob URL: ${blobUrl}`);
+			return blobUrl;
+		} else {
+			throw new Error("contentHtml is undefined");
+		}
+	} catch (error) {
+		console.error("Error in runGit:", error);
+		throw error;
+	}
 };
 
 export const run = async (username: string) => {
-	const contentHtml = await createAssistant();
-	const url = await runGit(username, contentHtml);
-	return url;
+	try {
+		const contentHtml = await createAssistant();
+		const url = await runGit(username, contentHtml);
+		return url;
+	} catch (error) {
+		console.error("Error in run:", error);
+		// Handle the error or return an error message as needed.
+		throw error;
+	}
 };
