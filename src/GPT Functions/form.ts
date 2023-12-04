31e15d2a9cb6f473deb403749b67055876b45ce2
RacquelNinaDennison <racquelnina@gmail.com>
2023-12-04 10:13:48 +0200

    Added some touch ups so that it can be customised

--- src/GPT Functions/form.ts
+++ src/GPT Functions/form.ts
@@ -32,7 +32,7 @@ Make the code as extensive as possible with minimal additions and inputs needed 
  
 Make sure that this follows the Material design principles and the principles that were provided. This should be a website that stands out. If extra colors are needed, add them, however, they should completement the colors already provided.
 
-Please ensure that the form is styled. 
+Please ensure that the form is styled.  When using pictures from unsplash, ensure that there are not weird symbols or numbers in the keyword field. Also, always ensure that they are styled well
 
 `;
 	return userDescriptionText;
